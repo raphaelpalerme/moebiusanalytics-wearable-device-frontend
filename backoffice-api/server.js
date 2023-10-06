8f9ee2488874ed7fb5f812a40fce99336d955146
raphaelpalerme <98458244+raphaelpalerme@users.noreply.github.com>
2023-10-06 21:07:43 +0200

    change listen

--- backoffice-api/server.js
+++ backoffice-api/server.js
@@ -315,6 +315,7 @@ const streamToBuffer = async (readableStream) => {
 };
 
 // Lancer le serveur sur le port 3000
-app.listen(3000, () => {
-    console.log("Serveur en écoute sur le port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur en écoute sur le port ${PORT}`);
 });
